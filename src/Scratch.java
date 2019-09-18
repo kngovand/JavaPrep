7b60d4977f5688d0a2a953b45bfc5fbb0a814578
K Duc <kngovand@gmail.com>
2019-09-18 03:28:44 -0600

    Selection sort added

--- src/Scratch.java
+++ src/Scratch.java
@@ -1,4 +1,37 @@
+import java.util.ArrayList;
+
 public class Scratch {
     public static void main(String[] args) {
+        int[] array = {15, 1, 50, 2};
+
+        for (int i = 1; i < array.length ; i++) {
+            int num = array[i];
+            int previous = i-1;
+
+            while(previous >= 0 && num < array[previous]) {
+                int temp = array[previous + 1];
+                array[previous + 1] = array[previous];
+                array[previous] = temp;
+                previous--;
+            }
+        }
+        for (int num: array
+             ) {
+            System.out.println(num);
+        }
+
+
+
+/*        for (int i = 0; i < array.length ; i++) {
+            for (int j = 0; j <array.length-1; j++) {
+                if(array[j] > array[j+1]){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }*/
+
+
     }
 }
--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,24 +1,24 @@
 import com.sun.javafx.geom.BaseBounds;
 
 import java.util.ArrayList;
-//  TO COMPLETE
 public class SelectionSort {
     public static void main(String[] args) {
         int[] array = {10, 5, 6, 18, 50};
-        selection(array);
-    }
-
-    public static int[] selection(int[] array) {
-        int min = 0, index = 0;
-        min = array[0];
-        for (int i = 0; i < array.length ; i++) {
-            if(array[i] < min) {
-                min = array[i];
-                System.out.println(min);
+        for (int i = 0; i <array.length-1 ; i++) {
+            int min = i;
+            for (int j = i+1; j < array.length ; j++) {
+                if(array[j] < array[min]) {
+                    min = j;
+                }
             }
+            int temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
         }
-        System.out.println(min);
-        return array;
+
+        for (int n: array) {
+            System.out.println(n);
+        }
     }
 
 }
