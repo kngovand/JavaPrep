d194e777a97f66492151b7d51d70e026342d86ff
K Duc <kngovand@gmail.com>
2019-09-22 13:48:55 -0600

    MorseCode rewritten with hashset - now complete

--- src/MorseCode.java
+++ src/MorseCode.java
@@ -3,41 +3,28 @@
 // We'll call such a concatenation, the transformation of a word.
 // Return the number of different transformations among all words we have.
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class MorseCode {
     public static void main (String[] args) {
-        String[] input = {"gin", "zen", "gig", "msg"};
+        String[] input = {"rwjje","aittjje","auyyn","lqtktn","lmjwn"};
         System.out.println(getMorse(input));
     }
 
     public static int getMorse(String[] words) {
-        int count = 0;
-        String[] output = new String[words.length];
         String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....",
                 "..", ".---","-.-", ".-..","--","-.","---", ".--.","--.-",
                 ".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
 
-        // To char array
-        for (int i = 0; i < words.length ; i++) {
-            String result = "";
-            char[] charArray = words[i].toCharArray();
-            // converting ASCII to morse code
-            for (int j = 0; j < charArray.length ; j++) {
-                Character c = charArray[j];
-                int value = c.charValue() - 97;
-                result += morse[value];
-            }
-            output[i] = result;
-            System.out.println(result);
-        }
-        //Counter for matching morse codes
-        for (int i = 0; i < output.length ; i++) {
-            for (int j = 0; j < output.length; j++) {
-                if(output[i] == (output[j])) {
-                    count++;
-                    break;
+            Set<String> seen = new HashSet();
+            for(String word: words) {
+                StringBuilder code = new StringBuilder();
+                for (char c : word.toCharArray()) {
+                    code.append(morse[c - 'a']);
                 }
+                seen.add(code.toString());
             }
-        }
-        return count;
+        return seen.size();
     }
 }
